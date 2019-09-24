430eac204fbfb590ef96cc61f43d8a948453ac76
Vladimir <vl.isakov@hotmail.com>
2019-09-24 22:42:15 +0200

    Implement Parser

--- Dataset.py
+++ Dataset.py
@@ -4,16 +4,17 @@ from recordclass import recordclass
 from collections import namedtuple
 from PIL import Image, ImageFilter, ImageOps, ImageChops
 import funcy
+import numpy
 
 
 class ImageDataset:
 
-    def __init__(self, index, labels, batch_size, splits, shuffle):
+    def __init__(self, source, index, labels, batch_size, splits, shuffle):
 
         self.data = None
-        self.index = index
+        self.source = source
+        self.index = (self.source + '/' + name for name in index)
         self.labels = labels
-
         self.batch_size = batch_size
         self.splits = splits
 
@@ -22,6 +23,11 @@ class ImageDataset:
 
         self._prepare_data(shuffle=shuffle)
 
+    @classmethod
+    def _init_entry(cls, sample, label):
+        Entry = namedtuple('Entry', ['sample', 'label'])
+        return Entry(sample, label)
+
     def __str__(self):
 
         return 'ImageDataset\nN Samples: {0}\nN Splits: {1}\nBatch Size: {2}'\
@@ -29,8 +35,7 @@ class ImageDataset:
 
     def _build_entries(self):
 
-        Entry = namedtuple('entry', 'sample label')
-        self.data = [Entry(sample=sample, label=label) for sample, label in zip(self.index, self.labels)]
+        self.data = [ImageDataset._init_entry(sample=sample, label=label) for sample, label in zip(self.index, self.labels)]
 
     def _build_splits(self, shuffle=False):
 
@@ -43,8 +48,8 @@ class ImageDataset:
 
         split_index = ([next(iter(data)) for _ in range(size)] for size in sizes)
 
-        Split = recordclass('Split', ['collection', 'key'])
-        splits = [Split(collection=i, key=k) for i, k in zip(split_index, self.splits.keys())]
+        Split = recordclass('Split', ['collection', 'name'])
+        splits = [Split(collection=i, name=k) for i, k in zip(split_index, self.splits.keys())]
 
         self.data = splits
 
@@ -65,6 +70,17 @@ class ImageTransformer:
     def __init__(self):
         self._transformations = []
 
+    def __str__(self):
+
+        info = []
+        separator = '\n'
+
+        for transformation in self.transformations:
+            info.append('Transformation - Name: {0}, Origin: {1}'.format(transformation.name, transformation.origin))
+
+        info = separator.join(info)
+        return info
+
     @property
     def transformations(self):
         return self._transformations
@@ -73,26 +89,74 @@ class ImageTransformer:
     def transformations(self, value):
         raise NotImplementedError
 
-    def add_unsharp_masking(self, radius=2, scale=1):
+    @classmethod
+    def _init_transformation(cls, func, name, origin):
+        Transformation = namedtuple('Transformation', ['func', 'name', 'origin'])
+        return Transformation(func, name, origin)
 
-        def gaussian_blur(img, rad=radius, sc=scale):
+    def add_unsharp_masking(self, radius=2, scale=1, origin=False):
+
+        name='unsharp_masking'
+
+        def unsharp_masking(img, rad=radius, sc=scale):
             gauss_img = img.filter(ImageFilter.GaussianBlur(radius=rad))
             return ImageChops.add(img, ImageChops.subtract(img, gauss_img, scale=sc))
 
-        self._transformations.append(gaussian_blur)
+        transformation = ImageTransformer._init_transformation(unsharp_masking, name, origin)
+        self._transformations.append(transformation)
 
-    def add_histogram_equalization(self):
-        self._transformations.append(ImageOps.equalize)
+    def add_histogram_equalization(self, origin=False):
 
-    def add_median_filter(self, size=3):
+        name = 'histogram_equalization'
+
+        transformation = ImageTransformer._init_transformation(ImageOps.equalize, name, origin)
+        self._transformations.append(transformation)
+
+    def add_median_filter(self, size=3, origin=False):
+
+        name = 'median_filter'
 
         def median_filter(img):
             return img.filter(ImageFilter.MedianFilter(size=size))
 
-        self._transformations.append(median_filter)
+        transformation = ImageTransformer._init_transformation(median_filter, name, origin)
+        self._transformations.append(transformation)
+
+    def add_resize(self, size, origin=False):
+
+        name = 'resize'
+
+        def resize(img, s = size):
+            return img.resize(s)
+
+        transformation = ImageTransformer._init_transformation(resize, name, origin)
+        self._transformations.append(transformation)
+
+
+    def add_grayscale(self, origin=False):
+
+        name = 'grayscale'
+
+        def grayscale(img):
+            return img.convert('L')
+
+        transformation = ImageTransformer._init_transformation(grayscale, name, origin)
+        self._transformations.append(transformation)
+
+    def add_rotation(self, angle, origin=False):
+
+        name = 'rotation'
+
+        def rotate(img):
+            return img.rotate(angle)
+
+        transformation = ImageTransformer._init_transformation(rotate, name, origin)
+        self._transformations.append(transformation)
+
 
 class Saver:
     pass
+
 
 class DataHandler:
 
@@ -104,41 +168,66 @@ class DataHandler:
 
     def transform(self):
 
-        for entry in self.dataset.data:
+        for split in self.dataset.data:
+            print(split.name)
+            for batch in split.collection:
 
-            sample = entry.sample
-            label = entry.label
-            origin = Image.open(sample)
+                transformed_batch = []
 
-            for transformation in self.transformer.transformations:
-                res = transformation(origin)
-                res.show()
+                for entry in batch:
+
+                    origin = Image.open(entry.sample)
+
+                    transformed = []
+
+                    for transformation in self.transformer.transformations:
+                        img = transformation.func(origin)
+                        if transformation.origin is True:
+                            origin = img
+                        else:
+                            transformed.append(img)
+
+                    transformed_batch.append(origin)
+                    transformed_batch.extend(transformed)
+
+                transformed_batch = [DataHandler.img2aray(img) for img in transformed_batch]
+                batch = numpy.stack(transformed_batch, axis=0)
+                print(batch.shape)
+
+    @staticmethod
+    def img2aray(img):
+        return numpy.asarray(img, dtype=numpy.uint8)
 
 
-
-data = os.listdir('C:/Users/C61124/trainingSample/0')
+data = os.listdir('/home/oem/PycharmProjects/CatsvsDogs/images')
 
 labels = [1 for _ in range(len(data))]
 
-dataset = ImageDataset(index=data,
+dataset = ImageDataset(source='/home/oem/PycharmProjects/CatsvsDogs/images',
+                        index=data,
                        labels=labels,
-                       batch_size=8,
+                       batch_size=2,
                        splits={'train': 0.6,
-                               'validation:': 0.2,
-                               'test': 0.2},
+                               'test': 0.4},
                        shuffle=True)
 
 transformer = ImageTransformer()
 
-transformer.add_unsharp_masking()
-transformer.add_histogram_equalization()
+transformer.add_resize((64,64),origin=True)
+transformer.add_grayscale(origin=True)
+transformer.add_unsharp_masking(origin=True)
+transformer.add_histogram_equalization(origin=True)
 transformer.add_median_filter()
+transformer.add_rotation(60)
 
 saver = Saver()
 handler = DataHandler(dataset=dataset,
                       transformer=transformer,
                       saver=saver)
 
+print(dataset)
+print(transformer)
+handler.transform()
 #cat = Image.open('C:/Users/C61124/ComputerVision/cat.jpg')
 #handler.transform()
 
